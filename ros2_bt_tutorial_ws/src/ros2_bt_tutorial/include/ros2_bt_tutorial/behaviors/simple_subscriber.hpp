aea2fc4890d8e018481c4358b7584613ffa84c2f
Luis Luna <luisluna12@utexas.edu>
2026-03-31 15:58:14 -0500

    Create Simple Subscriber Behavior

---
+++ ros2_bt_tutorial_ws/src/ros2_bt_tutorial/include/ros2_bt_tutorial/behaviors/simple_subscriber.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "behaviortree_ros2/bt_topic_sub_node.hpp"
+
+#include "std_msgs/msg/string.hpp"
+
+
+using String = typename std_msgs::msg::String;
+
+class SimpleSubscriber : public BT::RosTopicSubNode<String>
+{
+public:
+  SimpleSubscriber(const std::string& name, const BT::NodeConfig& config, const BT::RosNodeParams);
+
+  static BT::PortsList providedPorts();
+
+  BT::NodeStatus onTick(const String::SharedPtr& msg);
+};
---
+++ ros2_bt_tutorial_ws/src/ros2_bt_tutorial/src/simple_subscriber_bt_executable.cpp
@@ -0,0 +1,38 @@
+#include "ros2_bt_tutorial/behaviors/simple_subscriber.hpp"
+
+#include <ament_index_cpp/get_package_share_directory.hpp>
+
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+
+  rclcpp::Node::SharedPtr node = std::make_shared<rclcpp::Node>("simple_subscriber_bt_executer");
+  
+  BT::BehaviorTreeFactory factory;
+
+  BT::RosNodeParams simple_subscriber_params;
+  simple_subscriber_params.default_port_value = "/topic"; // Name of topic to subscribe to
+  simple_subscriber_params.nh = node; // Node to create subscriber
+
+  // Register simple subscriber behavior
+  factory.registerNodeType<SimpleSubscriber>("SimpleSubscriber", simple_subscriber_params);
+
+  // Find package share directory
+  std::string share_dir = ament_index_cpp::get_package_share_directory("ros2_bt_tutorial");
+  
+  BT::Tree tree = factory.createTreeFromFile(share_dir + "/trees/simple_subscriber_tree.xml");
+
+  // Ensure tree runs slow enough for ^C to work
+  while(rclcpp::ok()) {
+    BT::NodeStatus status = tree.tickOnce();
+
+    if (status != BT::NodeStatus::RUNNING) {
+      break;
+    }
+
+    tree.sleep(std::chrono::milliseconds(10));
+  } 
+  
+  return 0;
+}
